a943ed0e21daeba793603587b9817ddb1bb8725c
Ebrahim Shahrivar <eshahrivar@clearpath.ai>
2019-04-26 17:41:30 -0400

    conditional icp - working

--- blam_slam/src/BlamSlam.cc
+++ blam_slam/src/BlamSlam.cc
@@ -280,9 +280,9 @@ if (first_time) {
   } else {
     // No new loop closures - but was there a new key frame? If so, add new
     // points to the map.
-    ROS_INFO("no loop closure");
+    //ROS_INFO("no loop closure");
     if (new_keyframe) {
-      ROS_INFO("keyframe");
+      //ROS_INFO("keyframe");
       //localization_.MotionUpdate(gu::Transform3::Identity());
       localization_.TransformPointsToFixedFrame(*msg, msg_fixed.get());
       PointCloud::Ptr unused(new PointCloud);
